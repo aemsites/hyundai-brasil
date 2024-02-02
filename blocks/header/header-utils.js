e8590bd5da95edf212680f972cd9f0dfeb814992
Alex Carol <acarolcampre@adobe.com>
2024-02-02 17:11:20 +0100

    Allow hamburger to close on desktop

--- blocks/header/header-utils.js
+++ blocks/header/header-utils.js
@@ -96,7 +96,6 @@ export function enableHover(tabButton, block, button, tab, navPanel) {
 export function enableClick(tabButton, block, button, tab, navPanel) {
   tabButton.addEventListener('click', () => {
     const activeButton = block.querySelector('button.active');
-
     if (!activeButton) {
       button.classList.add('active');
       // add active class to parent li
@@ -106,7 +105,7 @@ export function enableClick(tabButton, block, button, tab, navPanel) {
         navPanel.after(tab.content);
         navPanel.nextSibling.classList.add('tab-active');
       }
-    } else if (activeButton !== tabButton) {
+    } else if (activeButton !== tabButton.firstChild) {
       activeButton.classList.remove('active');
       // remove active class from parent li
       activeButton.parentElement.classList.remove('active');
@@ -120,6 +119,14 @@ export function enableClick(tabButton, block, button, tab, navPanel) {
       if (tab.content) {
         tab.content.classList.add('active');
         navPanel.after(tab.content);
+      }
+    } else {
+      button.classList.remove('active');
+      // add active class to parent li
+      tabButton.classList.remove('active');
+      if (tab.content) {
+        tab.content.classList.remove('active');
+        navPanel.nextSibling.remove();
       }
     }
   });
