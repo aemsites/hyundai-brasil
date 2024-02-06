c87da4317cb8ed0ba5a0639e6ee0ff92828b81e1
Meet Kumar Jain <meetkjain83@gmail.com>
2024-02-06 11:24:53 +0530

    Updating nav-test as nav for testing so that we may not disturb the actual POC site

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -95,7 +95,7 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
 export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta).pathname : '/nav';
+  const navPath = navMeta ? new URL(navMeta).pathname : '/nav-test';
   const fragment = await loadFragment(navPath);
 
   // decorate nav DOM
