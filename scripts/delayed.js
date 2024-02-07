552323ba30d58bcc3d995cb2a8771d8462dc1e28
Alex Carol <acarolcampre@adobe.com>
2024-02-07 10:59:56 +0100

    Add handtalk widget

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,16 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './aem.js';
+import { loadScript, sampleRUM } from './aem.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-// add more delayed functionality here
+loadScript('https://plugin.handtalk.me/web/latest/handtalk.min.js').then(() => {
+  // eslint-disable-next-line no-new,no-undef
+  new HT({
+    avatar: 'MAYA',
+    align: 'bottom',
+    side: 'left',
+    highContrast: true,
+    token: 'b90cfe223dbe6f25e09584a3e5f36ebb',
+  });
+});
