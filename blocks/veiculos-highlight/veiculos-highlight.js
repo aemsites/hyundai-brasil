dcb60e91d6e7e478eb13c89e97bb50d47f68bb8c
Meet Kumar Jain <meetkjain83@gmail.com>
2024-02-09 03:41:52 +0530

    Further improvization on JS & CSS for the block

--- blocks/veiculos-highlight/veiculos-highlight.js
+++ blocks/veiculos-highlight/veiculos-highlight.js
@@ -4,20 +4,24 @@ export default function decorate(block) {
   // const leftDiv = div({ class: 'highlighted-vehicle' });
   const rightDiv = div({ class: 'vehicles-list' });
   let virtualDiv = 0;
+  let metadataDiv = 0;
   
   [...block.children].forEach((row, r) => {
     if(row.querySelector('img')) {
       if (r > 0) {
+        virtualDiv.appendChild(metadataDiv);
         rightDiv.appendChild(virtualDiv);
       }
+      metadataDiv = div();
       virtualDiv = div();
       virtualDiv.appendChild(row);
       row.classList.add('vehicle');
     } else {
       row.classList.add('metadata');
-      virtualDiv.appendChild(row);
+      metadataDiv.appendChild(row);
     }
   });
+  virtualDiv.appendChild(metadataDiv);
   rightDiv.appendChild(virtualDiv);
   console.log(virtualDiv);
   console.log(rightDiv.querySelector('div'));
