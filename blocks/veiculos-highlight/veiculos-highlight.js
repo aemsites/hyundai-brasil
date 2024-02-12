20c42c9cfd5d1beaf2cffd6fb9e55eed448f0663
meejain <37831850+meejain@users.noreply.github.com>
2024-02-12 17:15:04 +0530

    Block for "vehiculos" tab - When we hover over an image, its magnified image will be displayed on the left along with its metadata (#66)
    * Updating the JS logic for the block
    
    * Further improvization on JS & CSS for the block
    
    * Further improvization on CSS for the left Div
    
    * Further improvization on CSS for the left Div
    
    * Further improvization on CSS for the left Div
    
    * Further improvization on CSS for the right Div
    
    * Finalyzing the CSS
    
    * Fixing the lint issues
    
    * Changed the content to the original nav hence
    
    * fixing the movement of car on the left
    
    * Fixing the hover effect and right section images getting cut issue
    
    * Fixing the overlay issue of the names in the right hand side div post hovering
    
    * Fixng the gap between the rows and from the left Div as well
    
    * Fixng the gap between the rows and from the left Div as well

---
+++ blocks/veiculos-highlight/veiculos-highlight.js
@@ -0,0 +1,36 @@
+import { div } from '../../scripts/dom-helpers.js';
+
+export default function decorate(block) {
+  // const leftDiv = div({ class: 'highlighted-vehicle' });
+  const rightDiv = div({ class: 'vehicles-list' });
+  let virtualDiv = 0;
+  let metadataDiv = 0;
+  [...block.children].forEach((row, r) => {
+    if (row.querySelector('img')) {
+      if (r > 0) {
+        virtualDiv.appendChild(metadataDiv);
+        rightDiv.appendChild(virtualDiv);
+      }
+      metadataDiv = div();
+      virtualDiv = div();
+      virtualDiv.appendChild(row);
+      row.classList.add('vehicle');
+    } else {
+      row.classList.add('metadata');
+      metadataDiv.appendChild(row);
+    }
+  });
+  virtualDiv.appendChild(metadataDiv);
+  rightDiv.appendChild(virtualDiv);
+  const leftDiv = div({ class: 'highlighted-vehicle' }, rightDiv.querySelector('div').cloneNode(true));
+  rightDiv.querySelectorAll('div').forEach((car) => {
+    car.addEventListener('mouseover', () => {
+      leftDiv.replaceChildren(car.cloneNode(true));
+    });
+  });
+  block.replaceChildren(
+    // TODO add car info
+    leftDiv,
+    rightDiv,
+  );
+}
