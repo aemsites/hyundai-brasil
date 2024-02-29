c43ae56d1e821f27eb2ad697994b8664471e1d20
Meet Kumar Jain <meetkjain83@gmail.com>
2024-03-01 03:33:16 +0530

    Testing this on a duplicate nav file

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -101,7 +101,7 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
 export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta).pathname : '/nav';
+  const navPath = navMeta ? new URL(navMeta).pathname : '/nav-v5';
   const fragment = await loadFragment(navPath);
 
   // decorate nav DOM
